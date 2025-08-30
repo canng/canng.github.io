dabadb6ccd470005deb59c6c3d8e539c725d4230
canng <61179051+canng@users.noreply.github.com>
2025-08-30 08:58:20 +0000

    Deploying to gh-pages from @ canng/canng.github.io@3bbcdafe26cee1822777023a5131e5314ff1d065 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -79,15 +79,15 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/repositories/";
               },
-            },{id: "post-editing-a-special-issue-on-designing-resilient-cities-sustainability-mdpi",
+            },{id: "post-",
         
-          title: "📢 Editing a Special Issue on Designing Resilient Cities (Sustainability, MDPI)",
+          title: "",
         
         description: "",
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2025/SI-Sustainability/";
+            window.location.href = "/blog/2025/2025-07-01-SI-Sustainability/";
           
         },
       },{id: "books-the-godfather",
